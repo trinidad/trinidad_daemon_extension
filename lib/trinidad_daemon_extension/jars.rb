0c7ab5f67b6ba678af2c09986304a40f542ec9e5
kares <self@kares.org>
2013-01-14 16:00:19 +0100

    load jars instead of requiring them

--- lib/trinidad_daemon_extension/jars.rb
+++ lib/trinidad_daemon_extension/jars.rb
@@ -5,5 +5,5 @@ trinidad_libs =
   Trinidad::Extensions::Daemon::TRINIDAD_LIBS : 
   File.expand_path('../../../trinidad-libs', __FILE__)
 
-require File.join(trinidad_libs, 'akuma.jar')
-require File.join(trinidad_libs, 'jna.jar')
+load File.join(trinidad_libs, 'akuma.jar')
+load File.join(trinidad_libs, 'jna.jar')
